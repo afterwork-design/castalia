1d505790ba498fa557bf37e27dff42957b5768ce
wangpengfei <2601339907@qq.com>
2022-03-11 13:55:05 +0800

    feat: 添加卡片链接

--- src/components/resourceCard.tsx
+++ src/components/resourceCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import {Text, Box} from "@chakra-ui/react";
 import {ResourceItem} from "src/server";
 import {RounderBox, H3} from "./primitives";
@@ -11,11 +11,27 @@ interface Props {
 const ResourceCard: React.FC<Props> = ({
     site
 }) => {
+    const linkRef = useRef<HTMLAnchorElement>(null);
+    
+    const clickHandle = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+        const linkEle = linkRef.current;
+        if (linkEle && event.target !== linkEle) {
+            linkEle.click();
+        };
+    };
+
     return (
         <RounderBox
             display="flex"
             p="15px"
             columnGap="15px"
+            _hover={{
+                transform: "scale(0.9)",
+                bgColor: "#ebedec"
+            }}
+            transition="all 1s"
+            cursor="pointer"
+            onClick={clickHandle}
         >
             <Image
                 src="/avatar.svg"
@@ -24,8 +40,17 @@ const ResourceCard: React.FC<Props> = ({
                 height="80px"
             />
             <Box>
-                <H3>{site.name}</H3>
-                <Text mt="15px">{site.description}</Text>
+                <H3 fontSize={16}>
+                    <a
+                        ref={linkRef}
+                        href={site.url}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        {site.name}
+                    </a>
+                </H3>
+                <Text mt="15px" color="gray.400">{site.description}</Text>
             </Box>
         </RounderBox>
     );
