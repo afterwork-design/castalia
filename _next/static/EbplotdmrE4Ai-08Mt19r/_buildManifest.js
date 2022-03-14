44e9b8b801034304d25abb53894423c94a921463
pengfeiw <pengfeixc@sina.com>
2022-03-14 14:34:33 +0000

    Deploying to gh-pages from @ afterwork-design/castalia@8d8a2bd2a2d9d050dd11aa82bc05016b4f809120 🚀

--- _next/static/EbplotdmrE4Ai-08Mt19r/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/471-2177bd328c24c47e9cc3.js","static/chunks/pages/index-b3d69fa9d31ee6d8b7a2.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/f88ItiOcOQ4yfm7q_Afij/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/471-2177bd328c24c47e9cc3.js","static/chunks/pages/index-6ac0c71b49d79d7e3e44.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
