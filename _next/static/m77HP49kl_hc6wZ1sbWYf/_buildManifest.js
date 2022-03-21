a841a72f5c2e168c241f5f04bc5666ae4f91691c
pengfeiw <pengfeixc@sina.com>
2022-03-21 03:06:37 +0000

    Deploying to gh-pages from @ afterwork-design/castalia@5cd94fc97ace1a3b60b0c34d48cbfec19559bfb9 🚀

---
+++ _next/static/m77HP49kl_hc6wZ1sbWYf/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/741-9479d11c5f1b3916dc71.js","static/chunks/pages/index-3d95212973946c44e002.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
