eb773588fc9454c3b42fdbfbc309fda76a2763d9
tangweijuan <46316641+tangweijuan@users.noreply.github.com>
2022-08-06 05:04:09 +0000

    Deploying to gh-pages from @ afterwork-design/castalia@0ea0b07bf34d24c27e851dfee702c6a530b6d3f1 🚀

--- _next/static/Xcef7EOSgEOgYyrTlXUuS/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/741-099887b729b849aeae46.js","static/chunks/pages/index-43aca90ee5cfd50f43e4.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
